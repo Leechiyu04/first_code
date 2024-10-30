a6e495f5b3bd59254e2639001fbc7563eefd28dd
Leechiyu04 <goodlhu1@naver.com>
2024-10-30 10:14:09 +0900

    소스코드 first_code에 calc_area

--- first_code.py
+++ first_code.py
@@ -1,8 +1,6 @@
 print("hello world")
 print("This is the first code")
-# 1부터 10까지의 합을 계산하는 프로그램
-total = 0
-for i in range(1, 11):
-    total += i
 
-print("1부터 10까지의 합은:", total)
+def calc_area(x, y):
+    result = x * y
+    return result
